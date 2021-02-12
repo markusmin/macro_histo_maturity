4e61612c79162d86a5b662dd89f3ab4f9cce17ce
Markus Min <markus.min@gmail.com>
2021-02-12 11:58:50 -0800

    Updated Fig 4 to remove title, improve axes/legend

--- analysis/maturity_codes_cnry_stacked_barplot.R
+++ analysis/maturity_codes_cnry_stacked_barplot.R
@@ -12,12 +12,15 @@ mypalette <- brewer.pal(8, "Spectral")
 setEPS()
 postscript(here("figures", "Fig4.eps"), width = 10, height = 6)
 
-par(mar = c(4,4,4,9))
+par(mar = c(4,4,1,9))
 par(xpd=TRUE)
 barplot(as.matrix(cnry.codes),col=mypalette,width = c(cnry.n$N),space = 0)
-legend(6800,1.06,legend = c("Immature (1)","Maturing (2)","Vitellogenesis (3)","Fertilized (4)","Ripe (5)","Spent (6)","Resting (7)","Mature (8)"),
+legend(6800,0.9,legend = c("Immature (1)","Maturing (2)","Vitellogenesis (3)","Fertilized (4)","Ripe (5)","Spent (6)","Resting (7)","Mature (8)"),
        col=c(mypalette),pch=15,y.intersp = 2.1,bty="n",cex = 1.2)
-mtext("Proportion",side=2,outer=TRUE,line=-1.5)
-mtext("Canary Rockfish Maturity Code Usage",side=3,outer=TRUE,line=-3,cex=1.5)
+# Legend title
+text(7200,0.9, "Stage", cex = 1.3)
+mtext("Proportion",side=2,outer=TRUE,line=-1.5, cex = 1.4, at = 0.55)
+mtext("Year", side = 1, outer = TRUE, line = -1.5, cex = 1.4, at = 0.45)
+# mtext("Canary Rockfish Maturity Code Usage",side=3,outer=TRUE,line=-3,cex=1.5)
 
 dev.off()
